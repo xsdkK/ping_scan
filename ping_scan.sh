50c740f4a50474aa7aa54bf6e2727304a35858ea
xsdkK <2062979323@qq.com>
2026-05-11 16:57:16 +0800

    Update ping_scan.sh

--- ping_scan.sh
+++ ping_scan.sh
@@ -3,7 +3,7 @@
 for i in {1..254}
 do 
 	ip=192.168.85.$i
-	ping -c 1 -W 1 $ip > /dev/null 2>&1
+	ping -c 1 -W 0.1 $ip > /dev/null 2>&1
 	if [ $? -eq 0 ];then
 		echo "$ip up"
 	else 
@@ -17,7 +17,7 @@ do
 	for b in {1..254}
 	do
 		ip=10.17.$a.$b
-		ping -c 1 -W 1 $ip > /dev/null 2>&1
+		ping -c 1 -W 0.1 $ip > /dev/null 2>&1
 		if [ $? -eq 0 ];then 
 			echo "$ip up"
 		else
