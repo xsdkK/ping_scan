da43acd7a09f2522167b0f0824e7c076bd2d5e70
xsdkK <2062979323@qq.com>
2026-05-11 16:22:00 +0800

    Update ping_scan.sh
    添加对10.17.0.0/17地址状态的检测（up/down）

--- ping_scan.sh
+++ ping_scan.sh
@@ -10,3 +10,18 @@ do
 		echo "$ip down"
 	fi
 done
+
+
+for a in {1..128}
+do
+	for b in {1..254}
+	do
+		ip=10.17.$a.$bash
+		ping -c 1 -W 1 $ip > /dev/null 2>&1
+		if [ $? -eq 0 ];then 
+			echo "$ip up"
+		else
+		 	echo "$ip down"
+		fi 
+	done 
+done 
