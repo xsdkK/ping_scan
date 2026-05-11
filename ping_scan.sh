d1485318bfb96db631e4b5aae6f0a1866becfa0a
Jiamin Xie <2062979323@QQ.COM>
2026-05-11 17:03:53 +0800

    Add files via upload

--- ping_scan.sh
+++ ping_scan.sh
@@ -3,7 +3,7 @@
 for i in {1..254}
 do 
 	ip=192.168.85.$i
-	ping -c 1 -W 0.1 $ip > /dev/null 2>&1
+	ping -c 1 -W 1 $ip > /dev/null 2>&1
 	if [ $? -eq 0 ];then
 		echo "$ip up"
 	else 
@@ -12,16 +12,13 @@ do
 done
 
 
-for a in {1..128}
+for a in {1..254}
 do
-	for b in {1..254}
-	do
-		ip=10.17.$a.$b
-		ping -c 1 -W 0.1 $ip > /dev/null 2>&1
-		if [ $? -eq 0 ];then 
-			echo "$ip up"
-		else
-		 	echo "$ip down"
-		fi 
-	done 
+	ip=10.17.64.$a
+	ping -c 1 -W 1 $ip > /dev/null 2>&1
+	if [ $? -eq 0 ];then 
+		echo "$ip up"
+	else
+	 	echo "$ip down"
+	fi 
 done 
